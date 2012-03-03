5351aaed51177499b586d77339d4f714231aa71a
Ajay Narayan <username.ajay@gmail.com>
2012-03-03 03:50:35 -0800

    Basic Admin page and tables

--- webui/fatninja/models.py
+++ webui/fatninja/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#Tweet table : Holds the tweet text and meta data information.
+
+class Tweet(models.Model):
+      tweet_text  = models.charField(max_length=140)
+      tweet_stamp = models.DateTimeField('Tweet Stamp')
+
+class MetaData(models.Model):
+      search_key   = models.Charfield(max_length = 30)
+      search_stamp = models.DateTimeField('Query Stamp') 
--- webui/settings.py
+++ webui/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '/home/mask/python/cs221/sentiment-analyzer/webui/sqlite3.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -115,7 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fatninja'
+    'fatninja',
+    'django.contrib.admin'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
