b40a0ad3f820e08f4e1076b86b1381570a96aa49
Madhusudan.C.S <madhusudancs@gmail.com>
2012-03-14 08:16:45 -0700

    Reimplement the whole mapper and reducer to use the new custom vectorizer API.

--- analyzer/map_reduce.py
+++ analyzer/map_reduce.py
@@ -1,46 +1,89 @@
-import cPickle
-import json
-import os
-import re
-import requests
+#!/usr/bin/env python
+#
+# Copyright 2012 Ajay Narayan, Madhusudan C.S., Shobhit N.S.
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+
+import cPickle
+import os
+
+from disco.core import Job
 from disco.core import Params
 from disco.func import chain_reader
 
+
 import datasettings
-from disco.core import Job, result_iterator
-from sklearn.feature_extraction.text import CountVectorizer
+
+from vectorizer import Vectorizer
 
 
-class Tokenizer(CountVectorizer):
-      
-     def transform(self,tweet):
-         analyze = self.build_analyzer()
-         return analyze(tweet)
-      
-def map(page,params):
-    et = emitTokens() 
+def mapper(page, params):
+    """The mapper function for the MapReduce which analyzes tweets.
+
+    We do a word N-gram analysis after stripping accents, decoding to
+    UTF-8 and then removing stop words.
+
+    Args:
+        page: The number of the twitter search page for the query that
+           this mapper has to fetch and analyze.
+        params: The parameter object that holds the map reduce
+            initialization parameters. This contains the query string!
+    """
+    import json
+    import requests
+
+    trained_vectorizer = params.trained_vectorizer
     try:
-        r = requests.get("http://search.twitter.com/search.json",params={'q': "Apple", 'rpp': 100, 'page': '1'})
-        page_of_tweets = json.loads(r.text.decode(charset, charset_error))
+        r = requests.get(
+            'http://search.twitter.com/search.json',
+            params={'q': params.query, 'rpp': 100, 'page': page})
+        page_of_tweets = json.loads(
+            r.text.decode(trained_vectorizer.charset,
+                          trained_vectorizer.charset_error))
+        tweets = page_of_tweets.get('results')
+        if not tweets:
+          return
 
-        for itr in page_of_tweets['results']:
-            tweet_text = itr['text'] 
-            tweet_id   = itr['id']
-            tokens = et.transform(tweet_text) 
+        for tweet in tweets:
+            tweet_text = tweet['text']
+            tweet_id = tweet['id']
+            analyze = trained_vectorizer.build_analyzer()
+            tokens = analyze(tweet_text)
             for token in tokens:
-                yield  (u" ".join(token),(tweet_id,1))
+                yield  (u" ".join(token), (tweet_id, 1))
 
     except requests.ConnectionError:
         pass
+
+
+def reducer(klv, params):
+    """Reducer of the MapReduce framework that builds the Counts matrix.
+
+    This returns just the counts for each token, tweet id pair. We build
+    the actual matrix after we get the results from the reducer.
     
-def reduce(iter, params):
+    Args:
+        klv: The key and the list of values given as input to the reducer.
+        params: The parameters supplied while initializing the MapReduce.
+    """
     from disco.util import kvgroup
 
-    vectorizer = params.vectorizer
+    trained_vectorizer = params.trained_vectorizer
 
-    for token, count_tuple_list in kvgroup(sorted(iter)):
-        j = vectorizer.vocabulary_.get(token)
+    for token, count_tuple_list in kvgroup(sorted(klv)):
+        j = trained_vectorizer.vocabulary_.get(token)
         if j is not None:
             new_dict = {}
             for doc_id, count in count_tuple_list:
@@ -49,41 +92,26 @@ def reduce(iter, params):
                 else:
                     new_dict[doc_id] += count
             yield j, new_dict
-        else:
-            yield None, None
-        
-        
-if __name__ == '__main__':
-    from sklearn.feature_extraction.text import *
+
+
+def bootstrap():
+    """Bootstraps the MapReducer.
+    """
     path = os.path.join(datasettings.DATA_DIRECTORY, 'buckets.txt')
-    vectorizer_file = open(os.path.join(datasettings.DATA_DIRECTORY, 'vectorizer.pickle'))
-    vectorizer = cPickle.load(vectorizer_file)
-    job = Job().run(input=['tag://data:bigtxt'],
-                    map=map,
-                    reduce=reduce,
-                    mapreader=chain_reader,
-                    params=Params(vectorizer=vectorizer))
+    vectorizer_file = open(os.path.join(datasettings.DATA_DIRECTORY,
+                                        'vectorizer.pickle'))
 
-count = 0
-numCols = 0
-docsDict = {}
-rowsList = []
-colsList = []
-dataList = []
-for word, dict in result_iterator(job.wait(show=True)):
-    numCols += 1
-    for docID, tfidf in dict:
-        if docID not in docsDict:
-            docsDict[docID] = count
-            count += 1
-        dataList.append(tfidf)
-        rowsList.append(docsDict[docID])
-        colsList.append(word)
+    trained_vectorizer = cPickle.load(vectorizer_file)
+    job = Job()
+    job.run(input=['tag://data:buckets'], map=mapper, reduce=reducer,
+            map_reader=chain_reader, params=Params(query='Apple',
+            trained_vectorizer=trained_vectorizer),
+            required_modules=[('vectorizer',
+                               os.path.join(datasettings.PROJECT_ROOT,
+                                            'analyzer', 'vectorizer.py'),)])
 
-classifier_file = open(os.path.join(datasettings.DATA_DIRECTORY, 'classifier.pickle'))
-classifier = cPickle.load(classifier_file) 
+    return trained_vectorizer.build_feature_matrix(job)
 
-feature_vector = scipy.sparse.coo_matrix(
-            (dataList, (rowsList, colsList)), shape=(count, numCols))
-feature_vector = feature_vector.tocsr() 
-prediction = classifier.predict(feature_vector)
+
+if __name__ == '__main__':
+    bootstrap()
